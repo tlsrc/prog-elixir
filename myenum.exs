038569b0d9f758c58a0d712645a6443af15047fa
Tristan Lescut <tristanlescut@gmail.com>
2016-09-06 22:52:14 -0400

    Add take

--- myenum.exs
+++ myenum.exs
@@ -8,7 +8,7 @@ defmodule MyEnum do
     each(t, f)
   end
 
-  def filter(list, predicate), do: _filter([], list, predicate)
+  def filter(enum, predicate), do: _filter([], enum, predicate)
   defp _filter(m, [], _p), do: Enum.reverse(m)
   defp _filter(m, [h|t], p) do
     if(p.(h)) do
@@ -18,14 +18,19 @@ defmodule MyEnum do
     end
   end
 
-  def split(list, count) when count >= 0, do: _split([], list, count)
-  def split(list, count) when count < 0, do: _split_reverse([], Enum.reverse(list), -count)
+  def split(enum, count) when count >= 0, do: _split([], enum, count)
+  def split(enum, count) when count < 0, do: _split_reverse([], Enum.reverse(enum), -count)
   defp _split(a, b, 0), do: {Enum.reverse(a), b}
   defp _split(a, [h|t], c), do: _split([h|a], t, c-1)
   defp _split_reverse(a, b, 0), do: {Enum.reverse(b), a}
   defp _split_reverse(a, [h|t], c), do: _split_reverse([h|a], t, c-1)
 
-
+  def take(enum, count) when count >= 0, do: _take([], enum, count)
+  def take(enum, count) when count < 0, do: _take_reverse([], Enum.reverse(enum), -count)
+  defp _take(acc, _enum, 0), do: Enum.reverse(acc)
+  defp _take(acc, [h|t], c), do: _take([h|acc], t, c-1)
+  defp _take_reverse(acc, _enum, 0), do: acc
+  defp _take_reverse(acc, [h|t], c), do: _take_reverse([h|acc], t, c-1)
 
 
 end
