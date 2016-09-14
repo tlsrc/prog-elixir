9694bb934d9e5a510d4ff47980cd792a8defcdb2
Tristan Lescut <tristanlescut@gmail.com>
2016-09-13 23:30:08 -0400

    More strings and binaries exercises

--- strings-binaries.exs
+++ strings-binaries.exs
@@ -13,6 +13,21 @@ defmodule StrBin do
   defp _calc(num1, operand, num2, [h|t]), do: _calc(num1, operand, [h|num2], t)
 
   def parse(num_char_list) do
-    res = Enum.reverse(num_char_list) |> to_string |> String.strip |> String.to_integer
+    Enum.reverse(num_char_list) |> to_string |> String.strip |> String.to_integer
   end
+
+  def center(strs) do
+    max_length = strs |> Enum.map(&String.length/1) |> Enum.max
+    strs |> Enum.map(fn str -> _pad(str, max_length) end) |> Enum.map(&IO.puts/1)
+  end
+
+  defp _pad(str, padding) do
+    required_padding = padding - String.length(str)
+    required_padding = required_padding
+    _do_pad(str, required_padding)
+  end
+
+  defp _do_pad(str, n) when n <= 0, do: str
+  defp _do_pad(str, n), do: _do_pad(" "<>str<>" ", n-2)
+
 end
